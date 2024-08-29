2dc846c5641e30c47e44b61c2b386c5d33c87622
DJlazcano <jlazcanoaguilar@deloitte.com>
2024-08-29 16:55:27 -0600

    Add project files.

---
+++ Practice3/LBClasses/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice3.LBClasses
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public int ISBN { get; set; }
+        public bool IsCheckedOut { get; set; }
+
+        public Book(string title, int iSBN, bool isCheckedOut = false)
+        {
+            Title = title;
+            ISBN = iSBN;
+            IsCheckedOut = isCheckedOut;
+        }
+    }
+}
---
+++ Practice3/LBClasses/Library.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice3.LBClasses
+{
+    public class Library
+    {
+        public List<Book> books = new List<Book>();
+
+        public void AddBook(Book b)
+        {
+            books.Add(b);
+        }
+
+        public void RemoveBook(int isbn)
+        {
+            Book tempBook = books.Find(b => b.ISBN == isbn);
+            books.Remove(tempBook);
+            Console.WriteLine($"\nBook {tempBook.Title} removed.\n");
+        }
+    }
+}
